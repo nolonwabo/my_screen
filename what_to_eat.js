0c0ef6e0ed068e11a7139056d8512c7ef0cd9e43
Nolonwabo <nolonwabomahanjana13@gmail.com>
2017-04-28 17:11:31 +0200

    first commit

--- what_to_eat.js
+++ what_to_eat.js
@@ -9,41 +9,42 @@ var outPut2 = document.querySelector('.output2');
 //var userInfo = document.querySelector('.userInfo');
 //var userInfoTemp = Handlebars.compile(userInfo.innerHTML);
 
-var meals = [{
-    Price: 29.99,
-    Picture: "small.png"
-  },
-  {
-    Price: 24.99,
-    Picture: "1408971580697.png"
-  },
+var meals = [
   {
     Price: 26.99,
-    Picture: "ChickfilA-Spicy-Chicken-Sandwich.jpg"
+    Picture: "img/ChickfilA-Spicy-Chicken-Sandwich.jpg"
+  },
+  {
+      Price: 29.99,
+      Picture: "img/small.png"
   },
   {
     Price: 49.99,
-    Picture: "burger-menu.jpg"
+    Picture: "img/burger-menu.jpg"
   },
   {
     Price: 46.99,
-    Picture: "sejarah-Hamburger.jpg"
+    Picture: "img/sejarah-Hamburger.jpg"
   },
   {
     Price: 48.99,
-    Picture: "mi-tummy-com-wp-content-uploads-lazy-dog-cafe-westminster-ca-lazy-dog-burger-and-fries-x-jpg-_u8mv.jpg"
+    Picture: "img/mi-tummy-com-wp-content-uploads-lazy-dog-cafe-westminster-ca-lazy-dog-burger-and-fries-x-jpg-_u8mv.jpg"
   },
   {
     Price: 99.99,
-    Picture: "hwy-55-burgers-shakes.jpg"
+    Picture: "img/hwy-55-burgers-shakes.jpg"
   },
   {
     Price: 89.99,
-    Picture: "bkasia33698_background03.jpg"
+    Picture: "img/bkasia33698_background03.jpg"
   },
   {
     Price: 69.99,
-    Picture: "BistroBurger-afa0d5b4.jpeg"
+    Picture: "img/BistroBurger-afa0d5b4.jpeg"
+  },
+  {
+    Price: 24.99,
+    Picture: "img/1408971580697.png"
   }
 ]
 
@@ -63,35 +64,41 @@ var orderBtn = document.querySelector('.order');
 var firstName = document.querySelector('.firstName');
 var lastName = document.querySelector('.lastName');
 
-var burgerPrices = [];
-var person = [];
+//var person = [];
 submitBotton.addEventListener('click', function() {
-  console.log(firstName.value, lastName.value);
-  if (firstName.value === undefined && lastName.value === undefined) {
-    person.push({
+
+  var person = {};
+  if (firstName.value !== undefined && lastName.value !== undefined) {
+    person = {
       firstName: firstName.value,
       lastName: lastName.value
-    })
+    };
   }
+
+  var burgerPrices = [];
   for (var i = 0; i < meals.length; i++) {
     var myMeal = meals[i];
     var price = myMeal.Price;
     if (textBox.value >= myMeal.Price) {
       burgerPrices.push(myMeal)
-
     }
-
   }
-console.log(person);
-  var outputV = displayTemp({
-    meals: burgerPrices,
-    person: person
-  });
 
-  output.innerHTML = outputV;
+  console.log(person);
+  // var outputV = displayTemp({
+  //   meals: burgerPrices,
+  // });
+  //
+  // output.innerHTML = outputV;
+
+  burgerPrices = burgerPrices.sort(function(b1, b2){
+    return b1.Price - b2.Price;
+  })
+
 
   document.querySelector('.output').innerHTML = displayTemp({
-    meals: burgerPrices
+    meals: burgerPrices,
+    person: person
   });
 
 });
